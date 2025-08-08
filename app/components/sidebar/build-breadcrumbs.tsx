8f460c33c28dfd138c4eea04cccc8c7dbd991f31
abrulic1 <abrulic1@etf.unsa.ba>
2025-08-08 16:04:28 +0200

    fix in breadcrumbs building

--- app/components/sidebar/build-breadcrumbs.tsx
+++ app/components/sidebar/build-breadcrumbs.tsx
@@ -2,6 +2,7 @@ import { href } from "react-router"
 import { splitSlug } from "~/utils/split-slug"
 import type { SidebarSection } from "./sidebar"
 
+//TODO add tests
 export const buildBreadcrumb = (items: SidebarSection[], pathname: string) => {
 	let trail: string[] = []
 
