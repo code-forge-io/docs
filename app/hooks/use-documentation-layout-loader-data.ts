22ce1dfc720c5b81c5509a87a669b6fb7d061c07
abrulic1 <abrulic1@etf.unsa.ba>
2025-08-13 16:12:09 +0200

    small change

--- app/hooks/use-documentation-layout-loader-data.ts
+++ app/hooks/use-documentation-layout-loader-data.ts
@@ -1,8 +1,8 @@
 import { useRouteLoaderData } from "react-router"
-import type { loader } from "~/routes/documentation-layout"
+import type { Route } from "../routes/+types/documentation-layout"
 
 export const useDocumentationLayoutLoaderData = () => {
-	const data = useRouteLoaderData<typeof loader>("routes/documentation-layout")
+	const data = useRouteLoaderData<Route.ComponentProps["loaderData"]>("routes/documentation-layout")
 	if (!data) {
 		throw new Error(
 			"useDocumentationLayoutLoaderData must be used inside a route that is a child of 'documentation-layout' route"
