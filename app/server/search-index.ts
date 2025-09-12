cb8a2f3a0366c8ae4d244c6ab2393e1d373cac72
abrulic1 <abrulic1@etf.unsa.ba>
2025-09-12 17:26:30 +0200

    added small console log for debugging

--- app/server/search-index.ts
+++ app/server/search-index.ts
@@ -13,7 +13,11 @@ export async function preloadSearchIndexes() {
 		versions.map(async (version) => {
 			if (!searchIndexes.has(version)) {
 				const { allPages } = await loadContentCollections(version)
+				// biome-ignore lint/suspicious/noConsole:TODO remove this
+				console.log({ version, allPages })
 				const searchIndex = createSearchIndex(allPages)
+				// biome-ignore lint/suspicious/noConsole:TODO remove this
+				console.log({ searchIndex })
 				searchIndexes.set(version, searchIndex)
 			}
 		})
